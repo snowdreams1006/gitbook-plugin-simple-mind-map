a2d36fe93bad6265d49e25d010b9ce46122a69f6
snowdreams1006 <snowdreams1006@163.com>
2019-11-23 11:05:17 +0800

    fix injection

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 var readmoreConfig = {};
 
 module.exports = {
-    book: {
-        assets: "./assets",
-        js: [
-            "https://my.openwrite.cn/js/readmore.js",
-            "readmore.js"
-        ]
-    },
     hooks: {
         "init": function() {
             this.log.debug.ln('init', this.options.pluginsConfig.readmore);
 
             readmoreConfig = this.options.pluginsConfig.readmore;
         },
-        "page:before": function(page) {
-            this.log.debug.ln('page:before', page.content);
+        "page": function(page) {
+            this.log.debug.ln('page', JSON.stringify(page.content));
 
             if (readmoreConfig) {
-                var str = '<div id="vip-container">'+page.content+'</div>';
+                readmoreConfig.id = 'vip-container';
+                var str = `
+                <div id="vip-container">
+                    ${page.content}
+                </div>
+                <script src="https://my.openwrite.cn/js/readmore.js"></script>
+                <script>
+                    var isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
+                    if (!isMobile) {
+                        var btw = new BTWPlugin();
+                        btw.init(${JSON.stringify(readmoreConfig)});
+                    }
+                </script>`;
 
                 page.content = str;
             }
 
             return page;
-        },
-        "page": function(page) {
-            this.log.debug.ln('page', JSON.stringify(page.content));
-
-            return page;
-        },
-        "finish:before": function() {
-            this.log.debug.ln('finish:before', this.options.pluginsConfig.readmore);
-        },
-        "finish": function() {
-            this.log.debug.ln('finish', this.options.pluginsConfig.readmore);
         }
     }
 };
