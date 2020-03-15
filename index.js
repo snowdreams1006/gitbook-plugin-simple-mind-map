390a046ec230990caaf1f14659febd9962b15e81
snowdreams1006 <snowdreams1006@163.com>
2020-03-15 21:47:22 +0800

    test plugin

--- index.js
+++ index.js
@@ -8,16 +8,45 @@
  *  - Description: Gitbook plugin index
  *  - Create Time: 2020-03-15
  */
+
+const reg = /^\s*```\s{0,4}mind(?::(.+))?((.*[\r\n]+)+?)?\s*```$/im;
+const escapeHTML = str => str.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+
+const handler = page => {
+    this.log.debug.ln("page:before", this.options.pluginsConfig["simple-mind-map"]);
+
+    // let result;
+
+    // while ((result = reg.exec(page.content))) {
+    //     const [block, conditions, content] = result;
+    //     const {height, color, title} = (conditions || '').split(',').reduce((obj, cond) => {
+    //         const [key, value] = cond.split('=');
+    //         obj[key] = value || true;
+    //         return obj;
+    //     }, {});
+
+    //     page.content = page.content.replace(block, `<p class="mindmaps-wrapper" align="center"><svg
+    //         style="${height ? `height: ${height}px` : ''}"
+    //         class="mindmaps" ${color ? 'color="true"' : ''}
+    //         data-content="${escapeHTML(JSON.stringify(content))}"></svg>
+    //         ${title ? `<p align="center">${title}</p>` : ''}</p>`);
+    // }
+
+    return page;
+};
+
 module.exports = {
     book: {
-        assets: './assets',
-        css: ['plugin.css'],
-        js: ['plugin.js'],
+        assets: "./assets",
+        css: ["plugin.css"],
+        js: ["plugin.js"],
     },
     hooks: {
-        "page:before": function(page) {
-            this.log.debug.ln("init", this.options.pluginsConfig["simple-mind-map"]);
-        }
+        "page:before": handler
     },
     blocks: {
         simplemindmap: {
@@ -29,5 +58,5 @@ module.exports = {
                 return block;
             }
         }
-  }
+    }
 };
