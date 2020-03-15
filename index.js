eba561e29e52436a7cba2c84245717d447c48b84
snowdreams1006 <snowdreams1006@163.com>
2020-03-15 17:02:21 +0800

    add pluin

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/*
+ *  ===============================================================
+ *                      Not just programmers
+ *  ===============================================================
+ *
+ *  - Document: index.js
+ *  - Author: snowdreams1006
+ *  - Description: Gitbook plugin index
+ *  - Create Time: 2020-03-15
+ */
+
 module.exports = {
-    
+    book: {
+        assets: './assets',
+        css: ['plugin.css'],
+        js: ['plugin.js'],
+    },
+    hooks: {
+        "page:before": function(page) {
+            this.log.debug.ln("init", this.options.pluginsConfig["simple-mind-map"]);
+
+        }
+    }
+
 };
