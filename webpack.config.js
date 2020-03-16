a0933bf6bdc082662112c4c8f1e372bc9aca1c95
snowdreams1006 <snowdreams1006@163.com>
2020-03-16 10:57:05 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -6,73 +6,27 @@
  *  - Document: index.js
  *  - Author: snowdreams1006
  *  - Description: Gitbook plugin index
- *  - Create Time: 2020-03-15
  */
 
-const path = require('path');
+var path = require('path');
+
 module.exports = {
-  entry: './src/plugin.js',
-  mode: 'development',
-  devtool: 'inline-source-map',
+  entry: "./src/plugin.js",
+  mode: "development",
+  devtool: "inline-source-map",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
      rules: [
        {
          test: /\.css$/,
          use: [
-           'style-loader',
-           'css-loader'
+           "style-loader",
+           "css-loader"
          ]
        }
      ]
    }
 };
-
-// const path = require('path');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-// module.exports = {
-//     entry: './src/plugin.js',
-//     mode: 'development',
-//     devtool: 'inline-source-map',
-//     output: {
-//         publicPath: '',
-//         path: path.join(__dirname, 'dist'),
-//         filename: 'plugin.min.js',
-//     },
-//     resolve: {
-//         extensions: ['.js'],
-//         modules: [path.resolve(__dirname, 'src/'), 'node_modules'],
-//         alias: {
-//             'markmap-tree': 'markmap/lib/d3-flextree',
-//             'markmap-parser': 'markmap/lib/parse.markdown',
-//             'markmap-transform': 'markmap/lib/transform.headings',
-//             'markmap-mindmap': 'markmap/lib/view.mindmap',
-//         },
-//     },
-//     plugins: [
-//         new MiniCssExtractPlugin({
-//             filename: 'plugin.min.css',
-//         }),
-//     ],
-//     // module: {
-//     //     rules: [
-//     //         {test: /\.css$/, loader: [MiniCssExtractPlugin.loader, 'css-loader']},
-//     //         {test: /\.png$/, use: {loader: 'file-loader'}},
-//     //         {
-//     //             /** babel */
-//     //             test: /\.js?$/,
-//     //             exclude: /(node_modules|bower_components)/,
-//     //             use: {
-//     //                 loader: 'babel-loader',
-//     //                 options: {
-//     //                     presets: ['env', 'es2015'],
-//     //                 },
-//     //             },
-//     //         },
-//     //     ],
-//     // },
-// };
