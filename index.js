f8294783e8e9458a311288eb00c2856d256187d5
snowdreams1006 <snowdreams1006@163.com>
2024-07-28 11:41:37 +0800

    remove moidle

--- index.js
+++ index.js
@@ -11,19 +11,13 @@ module.exports = {
             this.log.debug.ln('page', JSON.stringify(page.content));
 
             if (readmoreConfig) {
-                var container = readmoreConfig.id;
-                if(container == undefined){
-                    container = ""
-                }
-
-                readmoreConfig.id = 'container';
                 var str = `
                 <div id="container">
                     ${page.content}
                 </div>
                 <script src="https://readmore.openwrite.cn/js/readmore-2.0.js" type="text/javascript"></script>
                 <script>
-                    var enablePlugin = false;
+                    var enablePlugin = false;                    
                     var allowDomain = ${JSON.stringify(readmoreConfig.allowDomain)};
                     if(allowDomain){
                         var currentDomain = location.hostname;
@@ -44,17 +38,8 @@ module.exports = {
                     }
                     
                     if(enablePlugin){
-                        var allowMobile = ${JSON.stringify(readmoreConfig.allowMobile)};
-                        if(allowDomain){
-
-                        }
-
-
-                        var isMobile = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
-                        if (!isMobile) {
-                            var btw = new BTWPlugin();
-                            btw.init(${JSON.stringify(readmoreConfig)});
-                        }
+                        var btw = new BTWPlugin();
+                        btw.init(${JSON.stringify(readmoreConfig)});
                     }
                 </script>`;
 
